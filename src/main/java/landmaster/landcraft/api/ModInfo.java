e2c1ca16cab03835eb5ef618cd0d8c35cf5434c1
Alex Thomson <LXGaming@users.noreply.github.com>
2019-02-06 15:42:09 +1300

    Move ModInfo class

--- src/main/java/landmaster/landcraft/api/ModInfo.java
+++ src/main/java/landmaster/landcraft/api/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
 	public static final String MODID = "landcraft";
 	public static final String NAME = "Land Craft";
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "2.2.1.2";
 	public static final String DEPENDS = "required-after:forge@[14.23.0.2498,);required-after:landcore@[1.5.0.0,);"
 	+ "after:jei;after:cofhcore";
 	
